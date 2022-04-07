c0528e56d40e40ed897247f3858c2e55840198c5
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-04-07 19:56:01 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -178,6 +178,46 @@ async def broadcast_messages(user_id, message):
     except Exception as e:
         return False, "Error"
 
+async def get_search_results(query, file_type=None, max_results=10, offset=0):
+    """For given query return (results, next_offset)"""
+
+    query = query.strip()
+    if not query:
+        raw_pattern = '.'
+    elif ' ' not in query:
+        raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
+    else:
+        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]')
+
+    try:
+        regex = re.compile(raw_pattern, flags=re.IGNORECASE)
+    except:
+        return []
+
+    if USE_CAPTION_FILTER:
+        filter = {'$or': [{'file_name': regex}, {'caption': regex}]}
+    else:
+        filter = {'file_name': regex}
+
+    if file_type:
+        filter['file_type'] = file_type
+
+    total_results = await Media.count_documents(filter)
+    next_offset = offset + max_results
+
+    if next_offset > total_results:
+        next_offset = ''
+
+    cursor = Media.find(filter)
+    # Sort by recent
+    cursor.sort('$natural', -1)
+    # Slice files according to offset and max results
+    cursor.skip(offset).limit(max_results)
+    # Get list of files
+    files = await cursor.to_list(length=max_results)
+
+    return files, next_offset
+
 async def save_file(media):
     """Save file in database"""
 
