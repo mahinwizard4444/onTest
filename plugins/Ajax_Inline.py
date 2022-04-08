fac0de8f82b40cfe197e89f76b70b4969ef636c5
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-04-08 22:19:23 +0530

    Update Ajax_Inline.py

--- plugins/Ajax_Inline.py
+++ plugins/Ajax_Inline.py
@@ -98,7 +98,7 @@ async def fil_mod(client, message):
       else:
           await m.edit("𝚄𝚂𝙴 :- /autofilter on 𝙾𝚁 /autofilter off")
 
-@Client.on_message(filters.group & filters.text & filters.incoming)
+@Client.on_message(filters.private & filters.text & filters.incoming)
 async def give_filter(client,message):
     group_id = message.chat.id
     name = message.text
@@ -1420,6 +1420,7 @@ async def auto_filter(client, msg, spoll=False):
     if imdb:
         cap = TEMPLATE.format(
             query = search,
+            mention = message.from_user.mention,
             title = imdb['title'],
             votes = imdb['votes'],
             aka = imdb["aka"],
@@ -1450,23 +1451,42 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"Here is what i found for your query {search}"
-    if imdb and imdb.get('poster'):
-        try:
-            await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
-                                      reply_markup=InlineKeyboardMarkup(btn))
-        except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
-            pic = imdb.get('poster')
-            poster = pic.replace('.jpg', "._V1_UX360.jpg")
-            await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
-        except Exception as e:
-            logger.exception(e)
-            await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
-    else:
-        await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
-    if spoll:
-        await msg.message.delete()
+        cap = f"<b><i>📟 Movie Name : {search}\n👩🏻‍💻 Requested By : {message.from_user.mention}\n🚀 Group : {message.chat.title}</i></b>"
 
+    if imdb and imdb.get('poster'):
+
+        try:
+
+            hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+
+            await asyncio.sleep(1500)
+
+            await hehe.delete()
+
+            await message.reply_text(text=f"<b>𝙵𝙸𝙻𝚃𝙴𝚁 𝙾𝙵 {search} 𝙲𝙻𝙾𝚂𝙴𝙳</b>", disable_notification = True)
+
+        except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+
+            pic = imdb.get('poster')
+
+            poster = pic.replace('.jpg', "._V1_UX360.jpg")
+
+            hmm = await message.reply_photo(photo=poster, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+
+            await asyncio.sleep(1500)
+
+            await hmm.edit_text(text=f"<b>𝙵𝙸𝙻𝚃𝙴𝚁 𝙾𝙵 {search} 𝙲𝙻𝙾𝚂𝙴𝙳</b>", disable_notification = True)
+
+        except Exception as e:
+
+            logger.exception(e)
+
+            fek = await message.reply_text(text=cap, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(btn))
+
+            await asyncio.sleep(1500)
+
+            await fek.edit_text(text=f"<b>𝙵𝙸𝙻𝚃𝙴𝚁 𝙾𝙵 {search} 𝙲𝙻𝙾𝚂𝙴𝙳</b>")   
+            
 async def advantage_spell_chok(msg):
     query = re.sub(
         r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
