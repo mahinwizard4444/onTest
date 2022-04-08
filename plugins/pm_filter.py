34c147d3bff564d125501f522f5b05d245c90e9f
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-04-09 00:07:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1418,7 +1418,7 @@ async def auto_filter(client, msg, spoll=False):
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     TEMPLATE = settings['template']
     if imdb:
-        cap = TEMPLATE.format(
+        cap = TEMPLATE.format(   
             query = search,
             title = imdb['title'],
             votes = imdb['votes'],
@@ -1450,28 +1450,47 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"Here is what i found for your query {search}"
+        cap = f"<b><i>📟 Movie Name : {search}\n👩🏻‍💻 Requested By : {message.from_user.mention}\n🚀 Group : {message.chat.title}</i></b>"
+
     if imdb and imdb.get('poster'):
+
         try:
-            await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
-                                      reply_markup=InlineKeyboardMarkup(btn))
+
+            hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+
+            await asyncio.sleep(1500)
+
+            await hehe.delete()
+
+            await message.reply_text(text=f"<b>𝙵𝙸𝙻𝚃𝙴𝚁 𝙾𝙵 {search} 𝙲𝙻𝙾𝚂𝙴𝙳</b>", disable_notification = True)
+
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+
             pic = imdb.get('poster')
+
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
-            await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+
+            hmm = await message.reply_photo(photo=poster, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+
+            await asyncio.sleep(1500)
+
+            await hmm.edit_text(text=f"<b>𝙵𝙸𝙻𝚃𝙴𝚁 𝙾𝙵 {search} 𝙲𝙻𝙾𝚂𝙴𝙳</b>", disable_notification = True)
+
         except Exception as e:
+
             logger.exception(e)
-            await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
-    else:
-        await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
-    if spoll:
-        await msg.message.delete()
+
+            fek = await message.reply_text(text=cap, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(btn))
+
+            await asyncio.sleep(1500)
+
+            await fek.edit_text(text=f"<b>𝙵𝙸𝙻𝚃𝙴𝚁 𝙾𝙵 {search} 𝙲𝙻𝙾𝚂𝙴𝙳</b>")
 
 async def advantage_spell_chok(msg):
     query = re.sub(
         r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
         "", msg.text, flags=re.IGNORECASE)  # plis contribute some common words
-    query = query.strip() + " movie"
+    query = query.strip() + "movie"
     g_s = await search_gagala(query)
     g_s += await search_gagala(msg.text)
     gs_parsed = []
