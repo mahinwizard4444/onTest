037b2dc29c35e39922756b5f1e011daca1a18a3b
OpusTechz <100134669+OpusTechz@users.noreply.github.com>
2022-04-09 15:45:44 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -30,8 +30,8 @@ SPELL_CHECK = {}
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def private_give_filter(client, message):
         await auto_filter(client, message)     
-    group_id = message.chat.id
-    name = message.text
+             group_id = message.chat.id
+             name = message.text
   
 
     keywords = await get_filters(group_id)
