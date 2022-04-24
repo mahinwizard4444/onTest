edcc22e576672bb61d48261d90ff0b8e2d557a16
alone-boy-A <103886669+alone-boy-A@users.noreply.github.com>
2022-04-24 13:41:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -490,6 +490,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 f'<b><i>{query.from_user.mention} Your File Is Ready ✨</i></b>\n\n'
                 f'<b><i>📟 Name : <a href=https://t.me/MWUpdatez>{title}</a></i></b>\n\n'
                 f'<b><i>🎗 Size : {size}</b></i>',
+                f'<i>⚡️Click The Below Button For Files.⚡️</i>',
                 True,
                 'html',
                 disable_web_page_preview=True,
