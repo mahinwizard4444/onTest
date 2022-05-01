b747841900dec736d2a323b14cbc363c11ecfeda
alone-boy-A <103886669+alone-boy-A@users.noreply.github.com>
2022-05-02 02:20:53 +0530

    Update __init__.py

--- Alita/__init__.py
+++ Alita/__init__.py
@@ -50,11 +50,11 @@ except Exception as ef:
     sysexit(1)
 
 LOGGER.info("------------------------")
-LOGGER.info("|      Tɪɢᴇʀ Sʜʀᴏғғ     |")
+LOGGER.info("|         ᗩᒍᗩ᙭         |")
 LOGGER.info("------------------------")
 LOGGER.info(f"Version: {Config.VERSION}")
 LOGGER.info(f"Owner: {str(Config.OWNER_ID)}")
-LOGGER.info("Source Code: https://github.com/Azanpopz/Eva-Alita\n")
+LOGGER.info("Source Code: https://github.com/Aadhi000/Ajax\n")
 
 # Account Related
 BOT_TOKEN = Config.BOT_TOKEN
