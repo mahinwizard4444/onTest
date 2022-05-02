cf0424c553f20199ba8ef213680668b7d8e5011e
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-05-03 00:32:23 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -4,10 +4,11 @@ from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from info import ADMINS
 import os
-from utils_1 import save_file
+from utils import save_file
+import pyromod.listen
 logger = logging.getLogger(__name__)
 lock = asyncio.Lock()
-from pyrogram.types import Message
+
 
 @Client.on_message(filters.command(['index', 'indexfiles']) & filters.user(ADMINS))
 async def index_files(bot, message):
@@ -16,13 +17,13 @@ async def index_files(bot, message):
         await message.reply('Wait until previous process complete.')
     else:
         while True:
-            last_msg = await message.ask(text = "Forward me last message of a channel which I should save to my database.\n\nYou can forward posts from any public channel, but for private channels bot should be an admin in the channel.\n\nMake sure to forward with quotes (Not as a copy)")
+            last_msg = await bot.ask(text = "Forward me last message of a channel which I should save to my database.\n\nYou can forward posts from any public channel, but for private channels bot should be an admin in the channel.\n\nMake sure to forward with quotes (Not as a copy)", chat_id = message.from_user.id)
             try:
                 last_msg_id = last_msg.forward_from_message_id
                 if last_msg.forward_from_chat.username:
                     chat_id = last_msg.forward_from_chat.username
                 else:
-                    chat_id=last_msg.forward_from_chat.id                    
+                    chat_id=last_msg.forward_from_chat.id
                 await bot.get_messages(chat_id, last_msg_id)
                 break
             except Exception as e:
