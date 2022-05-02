4e9661f481df85b590bbb41389cb03d1354664a7
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-05-02 15:13:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -393,11 +393,11 @@ async def settings(client, message):
             ],
             [
                 InlineKeyboardButton(
-                    '𝐁𝐎𝐓 𝐏𝐌',
+                    '𝐅𝐈𝐋𝐄𝐒',
                     callback_data=f'setgs#botpm#{settings["botpm"]}#{grp_id}',
                 ),
                 InlineKeyboardButton(
-                    '✅ 𝐘𝐄𝐒' if settings["botpm"] else '🗑️ 𝐍𝐎',
+                    '⚡ 𝐁𝐎𝐓 𝐏𝐌' if settings["botpm"] else '⚡ 𝐂𝐇𝐀𝐓',
                     callback_data=f'setgs#botpm#{settings["botpm"]}#{grp_id}',
                 ),
             ],
