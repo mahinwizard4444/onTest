25cd88921547806d40bf5ce17c8df15088a04eff
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-05-03 22:32:47 +0530

    Update extra.py

--- Aadhi/bot/plugins/extra.py
+++ Aadhi/bot/plugins/extra.py
@@ -1,22 +1,29 @@
-"""Telegram Ping / Pong Speed
-Syntax: .ping"""
-
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import filters
 import time
-import random
-from pyrogram import Client, filters
-from info import COMMAND_HAND_LER
-from Aadhi import StartTime
+import shutil, psutil
 from utils_bot import *
-@Client.on_message(filters.command('ping') & filters.private & ~filters.edited)
-async def ping(_, message):
-    start_t = time.time()
-    rm = await message.reply_text("...")
-    end_t = time.time()
-    time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
+from Aadhi import StartTime
 
-@Client.on_message(filters.command('stats') & filters.private & ~filters.edited)
+@Client.on_message(filters.command('ping') & filters.private & ~filters.edited)
 async def stats(bot, update):
   currentTime = readable_time((time.time() - StartTime))
-  botstats = f'<b>Bot Uptime:</b> {currentTime}'           
+  total, used, free = shutil.disk_usage('.')
+  total = get_readable_file_size(total)
+  used = get_readable_file_size(used)
+  free = get_readable_file_size(free)
+  sent = get_readable_file_size(psutil.net_io_counters().bytes_sent)
+  recv = get_readable_file_size(psutil.net_io_counters().bytes_recv)
+  cpuUsage = psutil.cpu_percent(interval=0.5)
+  memory = psutil.virtual_memory().percent
+  disk = psutil.disk_usage('/').percent
+  botstats = f'<b>Bot Uptime:</b> {currentTime}\n' \
+            f'<b>Total disk space:</b> {total}\n' \
+            f'<b>Used:</b> {used}  ' \
+            f'<b>Free:</b> {free}\n\n' \
+            f'📊Data Usage📊\n<b>Upload:</b> {sent}\n' \
+            f'<b>Down:</b> {recv}\n\n' \
+            f'<b>CPU:</b> {cpuUsage}% ' \
+            f'<b>RAM:</b> {memory}% ' \
+            f'<b>Disk:</b> {disk}%'
   await update.reply_text(botstats)
