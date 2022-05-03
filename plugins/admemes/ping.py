d4af5112849dec67c1147829a1bfb1dae12d4cf7
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-05-03 22:05:58 +0530

    Update ping.py

--- plugins/admemes/ping.py
+++ plugins/admemes/ping.py
@@ -17,7 +17,7 @@ async def ping(_, message):
     time_taken_s = (end_t - start_t) * 1000
     await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
 
-@Client.on_message(filters.command('ping') & filters.private & ~filters.edited)
+@Client.on_message(filters.command('stats') & filters.private & ~filters.edited)
 async def stats(bot, update):
   currentTime = readable_time((time.time() - StartTime))
   botstats = f'<b>Bot Uptime:</b> {currentTime}'           
