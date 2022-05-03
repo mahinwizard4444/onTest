cb5325d8671b0c31b6dd353a76ff451a96838e81
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-05-03 22:31:06 +0530

    Update ping.py

--- plugins/admemes/ping.py
+++ plugins/admemes/ping.py
@@ -5,19 +5,11 @@ import time
 import random
 from pyrogram import Client, filters
 from info import COMMAND_HAND_LER
-from Aadhi import StartTime
-from utils_bot import *
 
-@Client.on_message(filters.command('ping') & filters.private & ~filters.edited)
+@Client.on_message(filters.command("ping", COMMAND_HAND_LER) & f_onw_fliter)
 async def ping(_, message):
     start_t = time.time()
     rm = await message.reply_text("...")
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
     await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
-
-@Client.on_message(filters.command('stats') & filters.private & ~filters.edited)
-async def stats(bot, update):
-  currentTime = readable_time((time.time() - StartTime))
-  botstats = f'<b>Bot Uptime:</b> {currentTime}'           
-  await update.reply_text(botstats)
