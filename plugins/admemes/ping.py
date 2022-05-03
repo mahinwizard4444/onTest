9dd1bb5443995a41f149082c922d92a3d571c90e
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-05-03 21:58:37 +0530

    Update ping.py

--- plugins/admemes/ping.py
+++ plugins/admemes/ping.py
@@ -7,7 +7,6 @@ import time
 import random
 from pyrogram import Client, filters
 from info import COMMAND_HAND_LER
-from plugins.helper_functions.cust_p_filters import f_onw_fliter
 from Aadhi import StartTime
 
 @Client.on_message(filters.command("ping", COMMAND_HAND_LER) & f_onw_fliter)
