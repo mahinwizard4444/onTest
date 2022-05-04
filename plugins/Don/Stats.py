3cb1e50e9270c2bc12696de7f72d3adf9f716c09
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-05-04 15:06:59 +0530

    Update Stats.py

--- plugins/Don/Stats.py
+++ plugins/Don/Stats.py
@@ -3,15 +3,24 @@ import math
 import time
 import heroku3
 import requests
-
+from plugins.helper_functions.cust_p_filters import f_onw_fliter
 from pyrogram import Client, filters
 from database.users_chats_db import db
+from info import COMMAND_HAND_LER
 
 #=====================================================
 BOT_START_TIME = time.time()
 
 HEROKU_API_KEY = (os.environ.get("HEROKU_API_KEY", ""))
 #=====================================================
+
+@Client.on_message(filters.command("ping", COMMAND_HAND_LER) & f_onw_fliter)
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
 
 @Client.on_message(filters.command('ping'))
 async def bot_status(client,message):
