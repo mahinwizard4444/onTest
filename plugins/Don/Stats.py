2b4e625897ce44be730f9535321e010538a8b7a3
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-05-04 15:29:35 +0530

    Update Stats.py

--- plugins/Don/Stats.py
+++ plugins/Don/Stats.py
@@ -20,7 +20,14 @@ async def ping(_, message):
     rm = await message.reply_text("...")
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
+    uptime = time.strftime("%Hh | %Mm | %Ss", time.gmtime(time.time() - BOT_START_TIME))
     await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
+    await message.reply_text(
+        "༺𝙲𝚄𝚁𝚁𝙴𝙽𝚃 𝙱𝙾𝚃 𝚂𝚃𝙰𝚃𝚄𝚂༻\n\n"       
+        f"‹›⧽ 𝙱𝙾𝚃 𝚄𝙿𝚃𝙸𝙼𝙴 : {uptime}\n"       
+        quote=True,
+        parse_mode="md"
+    )
 
 async def bot_status(client,message):
     if HEROKU_API_KEY:
